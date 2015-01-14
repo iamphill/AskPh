9fe9b5db260eb87815de349a65c080ade1328bde
Phil Hughes <me@iamphill.com>
2015-01-14 11:48:14 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+        app = express(),
+      askph = require('./lib/askph.js');
+
+// Listen for the feed URL
+app.get('/feed', function (req, res) {
+  // Create new class
+  new askph(req, res);
+});
+
+// Start server
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('askph podcast app listening at http://%s:%s', host, port);
+});
