2636af7e5d37d32711d6a1739bb5d0dc4a5029ef
Phil Hughes <me@iamphill.com>
2015-01-19 12:22:01 +0000

    Download URL

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/feed', function (req, res) {
   new askph(req, res);
 });
 
+app.get('/:track_id.mp3', function (req, res) {
+  var request = require('request');
+  var settings = require('./lib/settings.json');
+  var fs = require('fs');
+
+  var url = 'https://api.soundcloud.com/tracks/' + req.params.track_id + '/stream?client_id=' + settings['client_id'];
+
+  request.get(url, function (error, response, body) {
+    res.end(body);
+  });
+});
+
 // Start server
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
