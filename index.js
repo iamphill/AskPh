d710eed606c26784ddb23605f8b9e2d9f8fef696
Phil Hughes <me@iamphill.com>
2015-01-29 13:21:21 +0000

    Byte range test

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.get('/feed', function (req, res) {
   new askph(req, res);
 });
 
+var data = '';
 app.get('/:track_id.mp3', function (req, res) {
   var request = require('request');
   var settings = require('./lib/settings.json');
@@ -25,8 +26,38 @@ app.get('/:track_id.mp3', function (req, res) {
 
   var url = 'https://api.soundcloud.com/tracks/' + req.params.track_id + '/stream?client_id=' + process.env['client_id'];
   console.log('REQ: Request for track ID `%s` received', req.params.track_id);
-  
-  request(url).pipe(res);
+
+  if (typeof req.headers.range !== 'undefined' && req.headers.range !== 'bytes=0-') {
+    function complete() {
+      var range = req.headers.range;
+      var total = data.length;
+      var parts = range.replace(/bytes=/, "").split("-");
+      var partialstart = parts[0];
+      var partialend = parts[1];
+
+      var start = parseInt(partialstart, 10);
+      var end = partialend ? parseInt(partialend, 10) : total;
+      var chunkSize = (end - start);
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunkSize,
+        "Content-Type": "audio/mp3"
+      });
+      res.write(data.slice(start, end) + '0', 'binary');
+    }
+    
+    if (data != '') {
+      complete();
+    } else {
+      request(url, function (err, response, body) {
+        data = body;
+        complete();
+      }); 
+    } 
+  } else {
+    request(url).pipe(res);
+  } 
 });
 
 // Start server
