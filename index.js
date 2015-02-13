c64dae8699e13b22137df35c8ed54ba7ca79e7f5
Phil Hughes <me@iamphill.com>
2015-02-13 09:10:09 +0000

    Linkify's the description

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ var express = require('express'),
  askphVideo = require('./lib/askph-video.js'),
       chalk = require('chalk');
 
+if(!String.linkify) {
+  String.prototype.linkify = function() {
+    // http://, https://, ftp://
+    var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+
+    // www. sans http:// or https://
+    var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+
+    // Email addresses
+    var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+    return this
+        .replace(urlPattern, '<a href="$&">$&</a>')
+        .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+        .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+  };
+}
+
 // Load env file
 if (process.env['client_id'] == undefined) {
   var env = require('node-env-file');
